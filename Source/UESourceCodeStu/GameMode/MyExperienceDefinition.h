e2f28aa3737e2d464a5a95959c3392f0498b3fc0
alwasywinder <1181612350@qq.com>
2024-10-09 14:39:13 +0800

    111

---
+++ Source/UESourceCodeStu/GameMode/MyExperienceDefinition.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "MyExperienceDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESOURCECODESTU_API UMyExperienceDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
